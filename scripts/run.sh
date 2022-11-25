6f54c3241f82106b3d0f66a8f5a8d16181fc3618
Derek Paulsen <derekpaulsensi@gmail.com>
2022-11-25 15:56:56 -0600

    clean up script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,36 +1,17 @@
 #!/bin/bash
-out='./out.json'
 script='./train_resnet.py'
+# the directory where everything will be output
+out_dir='/tmp'
+props=('1,0,0' '.5,0,0' '.5,.5,0' '0,1,0' '0,.5,0' '1,1,0' '.5,0,.5' '0,0,1' '0,0,.5' '1,0,1' '1,1,1')
 
 for i in 1 2 3 4 5
 do
-	# 100% orig BASELINE 
-	python3.8 $script --data_props '(1, 0, 0)' | tee -a $out
-	# 50% orig
-	python3.8 $script --data_props '(.5, 0, 0)' | tee -a $out 
-
-
-	# 50% orig + 50% syn
-	python3.8 $script  --data_props '(.5, .5, 0)' | tee -a $out
-	# 100% syn
-	python3.8 $script --data_props '(0, 1, 0)' | tee -a $out
-	# 50% syn
-	python3.8 $script --data_props '(0, .5, 0)' | tee -a $out
-	# 100% orig + 100% syn
-	python3.8 $script --data_props '(1, 1, 0)' | tee -a $out
-
-
-	# 50% orig + 50% syn2
-	python3.8 $script  --data_props '(.5, 0, .5)' | tee -a $out
-	# 100% syn2
-	python3.8 $script --data_props '(0, 0, 1)' | tee -a $out
-	# 50% syn2
-	python3.8 $script --data_props '(0, 0, .5)' | tee -a $out
-	# 100% orig + 100% syn2
-	python3.8 $script --data_props '(1, 0, 1)' | tee -a $out
-
-
-	# 100% orig + 100% syn + 100% syn2
-	python3.8 $script --data_props '(1, 1, 1)' | tee -a $out
+	for prop in ${props[@]}
+	do
+		## 100% orig BASELINE 
+		out=$(mktemp -p $out_dir --suffix="_$(basename $script).json")
+		echo "$out"
+		python3.8 $script --data_props "$prop" | tee -a $out
+	done
 done
 
