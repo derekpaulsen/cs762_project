e68af680e261d8ef2ecb527db6ed6f9c7d6d6acd
j-gorka <joe.gorka13@gmail.com>
2022-11-27 16:28:48 -0600

    Created train_resnet50, renamed train_resnet to train_resnet18

--- scripts/run_densenet121.sh
+++ scripts/run_densenet121.sh
@@ -5,16 +5,16 @@
 for i in seq 10
 do
         # 50% orig
-        python3.8 ./train_densenet.py --data_props '(.5, 1, 0, 0)' | tee -a out.json
+        python3.8 ./train_densenet121.py --data_props '(.5, 1, 0, 0)' | tee -a out.json
         # 50% orig + 50% syn
-        python3.8 ./train_densenet.py --data_props '(.5, 1, .5, 0)' | tee -a out.json
+        python3.8 ./train_densenet121.py --data_props '(.5, 1, .5, 0)' | tee -a out.json
         # 100% orig
-        python3.8 ./train_densenet.py --data_props '(1, 1, 0, 0)' | tee -a out.json
+        python3.8 ./train_densenet121.py --data_props '(1, 1, 0, 0)' | tee -a out.json
         # 100% syn
-        python3.8 ./train_densenet.py --data_props '(0, 1, 1, 0)' | tee -a out.json
+        python3.8 ./train_densenet121.py --data_props '(0, 1, 1, 0)' | tee -a out.json
         # 50% syn
-        python3.8 ./train_densenet.py --data_props '(0, 1, .5, 0)' | tee -a out.json
+        python3.8 ./train_densenet121.py --data_props '(0, 1, .5, 0)' | tee -a out.json
         # 100% orig + 100% syn
-        python3.8 ./train_densenet.py --data_props '(1, 1, 1, 0)' | tee -a out.json
+        python3.8 ./train_densenet121.py --data_props '(1, 1, 1, 0)' | tee -a out.json
 done
 
