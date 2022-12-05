c90df948fa5229e7508069def61f912ed0d65274
derekpaulsen <derekpaulsensi@gmail.com>
2022-12-05 16:03:22 -0600

    update plots

--- analysis/plot.py
+++ analysis/plot.py
@@ -20,13 +20,17 @@ def data_props_to_string(s):
     orig, syn1, syn2 = np.array(list(eval(s))) * 100
     return f'%{orig} orig, %{syn1} syn1, %{syn2} syn2'
 
-def plot_runs(ax, runs):
+def plot_runs(ax, runs, running_max=False):
     ymins = np.column_stack(runs.values).min(axis=1)
     ymaxs = np.column_stack(runs.values).max(axis=1)
     
     START_EPOCH = np.argmax(ymins >= ymaxs.max() - .1)
 
     for idx, acc in runs.items():
+
+        if running_max:
+            acc = np.maximum.accumulate(acc)
+
         ax.plot(np.arange(1, len(acc)+1), acc, label=data_props_to_string(idx))
     ax.set_xlabel = 'Epoch'
     ax.set_ylabel = 'Accuracy'
@@ -34,12 +38,12 @@ def plot_runs(ax, runs):
     ax.set_ylim(ymaxs.max() -.1, None)
 
 
-def make_figure(data, models, data_props):
+def make_figure(data, models, data_props, running_max=False):
     scale = 10
     fig, axes = plt.subplots(len(models), figsize=(1.75 * scale,  1 * scale))
     for m, ax in zip(models, axes.flatten()):
         d = data.loc[m]
-        plot_runs(ax, d.loc[data_props])
+        plot_runs(ax, d.loc[data_props], running_max)
         ax.set_title(m)
         ax.legend()
 
@@ -57,11 +61,15 @@ def main():
     fig, axes = make_figure(data['accuracy'], models, props)
     fig.savefig('./exp_res/resnets.png')
 
+    fig, axes = make_figure(data['accuracy'], models, props, running_max=True)
+    fig.savefig('./exp_res/resnets_running_max.png')
+
     models = ['resnet18', 'vgg16', 'densenet121']
-    props = ['1,0,0', '1,1,0', '1,0,1', '1,1,1']
     
     fig, axes = make_figure(data['accuracy'], models, props)
     fig.savefig('./exp_res/res_vgg_dense.png')
+    fig, axes = make_figure(data['accuracy'], models, props, running_max=True)
+    fig.savefig('./exp_res/res_vgg_dense_running_max.png')
 
 if __name__ == '__main__':
     main()
